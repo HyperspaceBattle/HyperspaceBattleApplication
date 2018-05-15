525b636dfd05e5e327ec7ba0cdfcbfa47614f139
HyperspaceBattle <38997264+HyperspaceBattle@users.noreply.github.com>
2018-05-15 06:03:24 -0400

    MInor Update
    minor scene fixes, added texture and scripts for grabpass distortion fx

---
+++ Assets/Component/animateOffset.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animateOffset : MonoBehaviour {
+ 
+     public bool on = true;
+ 
+     public float scrollSpeed;
+	 public float maxTime = 99999;
+	 private float offset;
+	 private float rate;
+     
+     private Material _material;
+ 
+     void Awake () {
+     
+         _material = GetComponent<Renderer>().material;
+
+		rate = scrollSpeed * Time.deltaTime;
+ 
+     }
+ 
+     void Update () {
+ 		
+         offset = Mathf.Min(maxTime, offset + rate);
+		_material.mainTextureOffset = new Vector2 (0, offset);
+ 
+     }
+ }
